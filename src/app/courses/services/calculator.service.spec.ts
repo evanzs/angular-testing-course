102b3a3d65318c9e28867c57b5e2734e5884d502
evanzs <evandrofidk@hotmail.com>
2023-04-13 12:55:56 -0300

    test(calculator) using depedency inject

--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -1,14 +1,20 @@
 import { CalculatorService } from './calculator.service';
 import { LoggerService } from './logger.service';
+import { TestBed} from '@angular/core/testing'
 describe('CalculadorService',()=>{
     let service:CalculatorService;
     let loggerSpy:any;
     beforeEach( () =>{
         console.log("before")
-
-         loggerSpy = jasmine.createSpyObj('LoggerService',["log"])
-         service = new CalculatorService(loggerSpy);
-
+        loggerSpy = jasmine.createSpyObj('LoggerService',["log"]);
+        TestBed.configureTestingModule({
+            providers:[
+                CalculatorService,
+                {provide:LoggerService,usevalue:loggerSpy}
+            ]
+        });
+       
+        service = TestBed.inject(CalculatorService)
     })
     it ('should add two numbers',() =>{     
         console.log("add test")
