460d035503e5d809674a04d9c2fd9de9ae8fb6d3
evanzs <evandrofidk@hotmail.com>
2023-04-14 21:54:41 -0300

    test(course-list) rotating_light 	:rotating_light:list all courses

--- src/app/courses/courses-card-list/courses-card-list.component.spec.ts
+++ src/app/courses/courses-card-list/courses-card-list.component.spec.ts
@@ -1,12 +1,17 @@
+import { By } from '@angular/platform-browser';
+import { COURSES } from './../../../../server/db-data';
 import { ComponentFixture, TestBed} from '@angular/core/testing';
 import {CoursesCardListComponent} from './courses-card-list.component';
 import {CoursesModule} from '../courses.module';
+import { setupCourses } from '../common/setup-test-data';
+import { DebugElement } from '@angular/core';
 
 
 
 describe('CoursesCardListComponent', () => {
   let fixture:ComponentFixture<CoursesCardListComponent>;
-let component:CoursesCardListComponent;
+  let component:CoursesCardListComponent;
+  let el  : DebugElement;
 beforeEach(async () =>{
 
  await TestBed.configureTestingModule({
@@ -16,6 +21,7 @@ beforeEach(async () =>{
 
  fixture = TestBed.createComponent(CoursesCardListComponent)
  component = fixture.componentInstance;
+ el = fixture.debugElement;
  fixture.detectChanges()
 })
 
@@ -26,7 +32,15 @@ beforeEach(async () =>{
 
   it("should display the course list", () => {
 
-    pending();
+      component.courses = setupCourses();      
+      //printa o elemento
+      console.log(el.nativeElement.outerHTML);
+      fixture.detectChanges();
+      //printa o elemento
+      console.log(el.nativeElement.outerHTML);
+      const cards = el.queryAll(By.css(".course-card"))
+      expect(cards).toBeTruthy();
+      expect(cards.length).toBe(12)
 
   });
 
